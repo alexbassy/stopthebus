d0fc9bb813c0c69342731840ee62d533f6cb6432
Alex Bass <alexpjbass@gmail.com>
2020-07-04 15:05:49 +0200

    remove unused helper

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,5 +1,2 @@
 export const range = (start = 0, end: number) =>
   new Array(end + 1 - start).fill(undefined).map((_, i) => i + start)
-
-export const getRandomValue = (list: any[]) =>
-  list[Math.floor(Math.random() * list.length)]
