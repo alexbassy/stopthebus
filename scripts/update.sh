2b96af7fe4d84e12e13fbef555253ef078a697fe
Alex Bass <alexpjbass@gmail.com>
2021-03-30 08:25:37 +0200

    tweak docker compose logging

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,6 @@ aws ecr get-login-password --region eu-central-1 | docker login --username AWS -
 echo "Building new image"
 REGISTRY=197098829537.dkr.ecr.eu-central-1.amazonaws.com docker-compose -f docker-compose.prod.yml build
 echo "Bringing down containers"
-docker-compose -f docker-compse.prod.yml down --remove-orphans
+docker-compose -f docker-compose.prod.yml down --remove-orphans
 echo "Relaunching"
-docker-compose -f docker-compse.prod.yml up --detach
+docker-compose -f docker-compose.prod.yml up --detach
