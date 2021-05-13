a591fbf68718f65cad39a2cb7f44fce7d2b470bb
Alex Bass <alexpjbass@gmail.com>
2021-05-13 11:25:29 +0200

    prune unused images
    Signed-off-by: Alex Bass <alexpjbass@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "Logging into ECR"
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 197098829537.dkr.ecr.eu-central-1.amazonaws.com
-echo "Building new image"
-REGISTRY=197098829537.dkr.ecr.eu-central-1.amazonaws.com docker-compose -f docker-compose.prod.yml build
+echo "Pruning unused images"
+docker image prune -a -f;
+
+echo "Pulling latest"
+docker-compose -f docker-compose.prod.yml pull; 
+
 echo "Bringing down containers"
 docker-compose -f docker-compose.prod.yml down --remove-orphans
+
 echo "Relaunching"
-docker-compose -f docker-compose.prod.yml up --detach
+docker-compose -f docker-compose.prod.yml up --detach;
